7af8865b6edc6a684c85c7179d469bd7d2eb7e3b
AshithShetty <ashithdshetty1101@gmail.com>
2023-04-11 02:18:46 +0530

    query string

--- starterpack/node-express-course/02-express-tutorial/app.js
+++ starterpack/node-express-course/02-express-tutorial/app.js
@@ -25,6 +25,27 @@ app.get('/api/products/:productID',(req,res)=>{
     res.json(singleProduct)
 })
 
+app.get('/api/v1/query',(req,res)=>{
+    const {search, limit}=req.query
+    let sortedProducts = [...products]
+
+    if(search){
+        sortedProducts= sortedProducts.filter((product)=>{
+            return product.name.startsWith(search)
+        })
+    }
+
+    if(limit){
+        sortedProducts=sortedProducts.slice(0,Number(limit))
+    }
+
+    if(sortedProducts.length<1){
+        return res.status(200).json([{success:true,data:[]}])
+    }
+
+    res.status(200).json(sortedProducts)
+})
+
 
 
 app.listen(5000,()=>{
